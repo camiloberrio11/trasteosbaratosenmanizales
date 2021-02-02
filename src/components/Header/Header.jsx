6c835c9c1999a3d93fd7f1bb6889404fb08eae95
camiloberrio <camiloberriouribe@gmail.com>
2021-02-01 21:46:47 -0500

    Se realiza cambio en el header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,15 @@ const Header = () => {
   return (
     <div className="header-layout">
       <div className="sec-heading light mx-auto">
-        <h2 className="animate__animated animate__backInLeft">Trasteos - Acarreos Manizales</h2>
+        <h2>Trasteos - Acarreos Manizales</h2>
+        <a
+          className="animate__animated animate__backInDown partner-info phone-contact "
+          href="http://wa.me/+573186234572?text=Hola, buen día! Estoy pendiente de realizar un acarreo y quisiera recibir más información. Gracias"
+          target="_blank"
+          rel="noreferrer"
+        >
+          3186234572
+        </a>
       </div>
     </div>
   );
