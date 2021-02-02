07a9bd0ffc55d3556df8626a0c1f03cff8f7d02c
camiloberrio <camiloberriouribe@gmail.com>
2021-02-01 21:31:57 -0500

    Se organiza colores y archivos javascript

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import BannerContacto from './components/BannerContacto/BannerContacto';
 import BotonWhatsapp from './components/BotonWhatsapp/BotonWhatsapp';
+import { Helmet } from 'react-helmet';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import ListadoServicios from './components/ListadoServicios/ListadoServicios';
@@ -7,6 +8,16 @@ import ListadoServicios from './components/ListadoServicios/ListadoServicios';
 function App() {
   return (
     <div id="main-wrapper">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Trasteos Manizales | Sitio web</title>
+        <link rel="preload" />
+        <meta
+          name="description"
+          content="this page is trasteos manizales and trasteos manizales, economicos, baratos en manizales"
+        />
+        <link rel="canonical" href="https://trasteosenmanizales.com/" />
+      </Helmet>
       <Header />
       <BotonWhatsapp />
       <ListadoServicios />
